71d5dfd54351db67fbafa730d4717b0b6426df59
jeihunmammadzada <jeihun.mammadzada@gmail.com>
2023-11-02 20:54:33 +0400

    Last project added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Demo Next App',
+    default: 'Demo application',
+  },
+  description: 'Demo application of NextJS 14',
+}
 
 export default function RootLayout({
   children,
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -8,15 +9,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', 1)
     if(term){
       params.set("query", term);
     }else{
       params.delete("query")
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
